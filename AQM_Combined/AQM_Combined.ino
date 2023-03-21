75a50ecb21c205c490b86488633ad6e257bcd2b3
Stephen Rey Agustinez <58624490+stpn120400@users.noreply.github.com>
2023-03-22 01:52:23 +0800

    Add files via upload

---
+++ AQM_Combined/AQM_Combined.ino
@@ -0,0 +1,187 @@
+/*
+  For Thesis Purposes
+  PROPONENTS:
+  Absurda, Cristine
+  Agustinez, Stephen Rey G.
+  Amolato, Glen Rose
+  Wendam, Sandra                
+*/
+
+// Serial Port connections for PM2.5 Sensor
+#define RXD2 16 // To sensor TXD
+#define TXD2 17 // To sensor RXD
+
+// Serial Port connections for DHT11 Sensor
+#include <DFRobot_DHT11.h>
+#define DHT11_PIN 27
+
+// Serial Port connections for MQ7 Sensor
+#include <MQUnifiedsensor.h>
+/************************Hardware Related Data************************************/
+#define         Board                   ("ESP32")
+#define         Pin                     (25)  //Analog input 3 of your arduino
+/***********************Software Related Data************************************/
+#define         Type                    ("MQ-7") //MQ7
+#define         Voltage_Resolution      (3.3)
+#define         ADC_Bit_Resolution      (12) // For arduino UNO/MEGA/NANO
+#define         RatioMQ7CleanAir        (27.5) //RS / R0 = 9.83 ppm 
+
+/*****************************Globals***********************************************/
+MQUnifiedsensor MQ7(Board, Voltage_Resolution, ADC_Bit_Resolution, Pin, Type);
+DFRobot_DHT11 DHT;
+/*****************************Globals***********************************************/
+
+void setup() {
+  // Start Serial Monitor
+  Serial.begin(115200);
+
+  // Set up UART connection for PM2.5
+  Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
+
+  //Init the serial port communication - to debug the library
+  //Serial.begin(9600); //Init serial port
+
+  //Set math model to calculate the PPM concentration and the value of constants
+  MQ7.setRegressionMethod(1); //_PPM =  a*ratio^b
+  MQ7.setA(99.042); MQ7.setB(-1.518); // Configure the equation to calculate CO concentration value
+   /*
+    Exponential regression:
+  GAS     | a      | b
+  H2      | 69.014  | -1.374
+  LPG     | 700000000 | -7.703
+  CH4     | 60000000000000 | -10.54
+  CO      | 99.042 | -1.518
+  Alcohol | 40000000000000000 | -12.35
+  */
+  
+  /*****************************  MQ Init ********************************************/ 
+  //Remarks: Configure the pin of arduino as input.
+  /************************************************************************************/ 
+  MQ7.init(); 
+  /* 
+    //If the RL value is different from 10K please assign your RL value with the following method:
+    MQ7.setRL(10);
+  */
+  /*****************************  MQ CAlibration ********************************************/ 
+  // Explanation: 
+   // In this routine the sensor will measure the resistance of the sensor supposedly before being pre-heated
+  // and on clean air (Calibration conditions), setting up R0 value.
+  // We recomend executing this routine only on setup in laboratory conditions.
+  // This routine does not need to be executed on each restart, you can load your R0 value from eeprom.
+  Serial.print("Calibrating please wait.");
+  float calcR0 = 0;
+  for(int i = 1; i<=10; i ++)
+  {
+    MQ7.update(); // Update data, the arduino will read the voltage from the analog pin
+    calcR0 += MQ7.calibrate(RatioMQ7CleanAir);
+    Serial.print(".");
+  }
+  MQ7.setR0(calcR0/10);
+  Serial.println("  done!.");
+  
+  if(isinf(calcR0)) {Serial.println("Warning: Conection issue, R0 is infinite (Open circuit detected) please check your wiring and supply"); while(1);}
+  if(calcR0 == 0){Serial.println("Warning: Conection issue found, R0 is zero (Analog pin shorts to ground) please check your wiring and supply"); while(1);}
+  /*****************************  MQ CAlibration ********************************************/ 
+  MQ7.serialDebug(true);
+}
+
+struct pms5003data {
+  uint16_t framelen;
+  uint16_t pm10_standard, pm25_standard, pm100_standard;
+  uint16_t pm10_env, pm25_env, pm100_env;
+  uint16_t particles_03um, particles_05um, particles_10um, particles_25um, particles_50um, particles_100um;
+  uint16_t unused;
+  uint16_t checksum;
+};
+
+struct pms5003data data;
+
+void loop() {
+  if (readPMSdata(&Serial1)) {
+    // reading data was successful!
+    Serial.println();
+    Serial.println("---------------------------------------");
+    Serial.println("Concentration Units (standard)");
+    Serial.print("PM 1.0: "); Serial.print(data.pm10_standard);
+    Serial.print("\t\tPM 2.5: "); Serial.print(data.pm25_standard);
+    Serial.print("\t\tPM 10: "); Serial.println(data.pm100_standard);
+    Serial.println("---------------------------------------");
+    Serial.println("Concentration Units (environmental)");
+    Serial.print("PM 1.0: "); Serial.print(data.pm10_env);
+    Serial.print("\t\tPM 2.5: "); Serial.print(data.pm25_env);
+    Serial.print("\t\tPM 10: "); Serial.println(data.pm100_env);
+    Serial.println("---------------------------------------");
+    Serial.print("Particles > 0.3um / 0.1L air:"); Serial.println(data.particles_03um);
+    Serial.print("Particles > 0.5um / 0.1L air:"); Serial.println(data.particles_05um);
+    Serial.print("Particles > 1.0um / 0.1L air:"); Serial.println(data.particles_10um);
+    Serial.print("Particles > 2.5um / 0.1L air:"); Serial.println(data.particles_25um);
+    Serial.print("Particles > 5.0um / 0.1L air:"); Serial.println(data.particles_50um);
+    Serial.print("Particles > 10.0 um / 0.1L air:"); Serial.println(data.particles_100um);
+    Serial.println("---------------------------------------");  
+
+    MQ7.update(); // Update data, the arduino will read the voltage from the analog pin
+    MQ7.readSensor(); // Sensor will read PPM concentration using the model, a and b values set previously or from the setup
+    MQ7.serialDebug(); // Will print the table on the serial port
+   // delay(500); //Sampling frequency
+
+    DHT.read(DHT11_PIN);
+    Serial.print("temp:");
+    Serial.print(DHT.temperature);
+    Serial.print("  humi:");
+    Serial.println(DHT.humidity);    
+  }
+
+  else{
+    Serial.println("Can't Obtain Data!");    
+  }
+}
+
+boolean readPMSdata(Stream *s) {
+  if (! s->available()) {
+    return false;
+  }
+
+  // Read a byte at a time until we get to the special '0x42' start-byte
+  if (s->peek() != 0x42) {
+    s->read();
+    return false;
+  }
+
+  // Now read all 32 bytes
+  if (s->available() < 32) {
+    return false;
+  }
+
+  uint8_t buffer[32];
+  uint16_t sum = 0;
+  s->readBytes(buffer, 32);
+
+  // get checksum ready
+  for (uint8_t i = 0; i < 30; i++) {
+    sum += buffer[i];
+  }
+
+  /* debugging
+    for (uint8_t i=2; i<32; i++) {
+    Serial.print("0x"); Serial.print(buffer[i], HEX); Serial.print(", ");
+    }
+    Serial.println();
+  */
+
+  // The data comes in endian'd, this solves it so it works on all platforms
+  uint16_t buffer_u16[15];
+  for (uint8_t i = 0; i < 15; i++) {
+    buffer_u16[i] = buffer[2 + i * 2 + 1];
+    buffer_u16[i] += (buffer[2 + i * 2] << 8);
+  }
+
+  // put it into a nice struct :)
+  memcpy((void *)&data, (void *)buffer_u16, 30);
+
+  if (sum != data.checksum) {
+    Serial.println("Checksum failure");
+    return false;
+  }
+  // success!
+  return true;
+}
