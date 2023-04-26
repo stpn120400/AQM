29a7b8d645594bc625881a7fdfe8cf0c69144a99
Stephen Rey Agustinez <58624490+stpn120400@users.noreply.github.com>
2023-04-26 22:24:10 +0800

    Update ustpAQM.ino

--- ustpAQM.ino
+++ ustpAQM.ino
@@ -7,11 +7,17 @@
   Wendam, Sandra                
 */
 
-#include "WiFi.h"
+//Last UPDATE: April 26, 2023
+
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <HTTPClient.h>
 #include "DHT.h"
+#include "Adafruit_Sensor.h"
 #define RXD2 16  // to sensor TX
 #define TXD2 17  // to sensor RX
 #define DHT11PIN 18
+#define pmsSerial Serial1
 
 
 DHT dht(DHT11PIN, DHT11);
@@ -20,15 +26,14 @@ const char* ssid = "Denzel_HomeNet";
 const char* password = "DenzelHomeNet2022";
 const char* host = "ustpapm.000webhostapp.com";  // hostname
 
-float CO = 0.00;
-const char* aqi_status;
-float humidity;
-float temperature;
-int PM1, PM2_5, PM10;
-int PM25_aqival;
-String send_Status_Read_DHT11 = "";
-String send_Status_Read_PMS5003 = "";
+// REPLACE with your Domain name and URL path or IP address with path
+const char* serverName = "https://ustpapm.000webhostapp.com/post-esp-data.php";
 
+// Keep this API Key value to be compatible with the PHP code provided in the project page.
+// If you change the apiKeyValue value, the PHP file /post-esp-data.php also needs to have the same key
+String apiKeyValue = "tPmAT5Ab3j7F9";
+
+int WiFiTimeoutCounter = 0;
 
 struct pms5003data {
   uint16_t framelen;
@@ -44,217 +49,84 @@ struct pms5003data data;
 void connect_WiFi() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi ..");
+  Serial.print("\nConnecting to WiFi ..");
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(1000);
+    delay(500);
+    Serial.print(".");
+    WiFiTimeoutCounter++;
+    if (WiFiTimeoutCounter >= 60) {  //after 30 seconds timeout - reset board
+      reboot();
+    }
   }
   Serial.println("\nConnected to the WiFi network");
   Serial.print("Local ESP32 IP: ");
   Serial.println(WiFi.localIP());
 }
 
-void get_DHT11_sensor_data() {
+void get_DHT11_sensor_data(float* x, float* y) {
   Serial.println();
   Serial.println("-----Getting DHT11 Sensor Data-----");
-
   //Reading DHT11 Data
-  humidity = dht.readHumidity();
-  temperature = dht.readTemperature();
+  *x = dht.readHumidity();
+  *y = dht.readTemperature();
 
   // Check if any reads failed.
-  if (isnan(temperature) || isnan(humidity)) {
+  if (isnan(*y) || isnan(*x)) {
     Serial.println("Failed to read from DHT sensor!");
-    temperature = 0.00;
-    humidity = 0;
-    send_Status_Read_DHT11 = "FAILED";
+    *x = 0.00;
+    *y = 0;
+    Serial.println("FAILED");
+    return;
   } else {
-    send_Status_Read_DHT11 = "SUCCEED";
-  }
-}
-
-void get_PMS5003_sensor_data() {
-  Serial.println("-----Getting PMS5003 Sensor Data-----");
-  if (readPMSdata(&Serial1)) {
-    PM1 = data.pm10_env;
-    PM2_5 = data.pm25_env;
-    PM10 = data.pm100_env;
-    PM25_aqival = calcAQI25(PM2_5);
-    send_Status_Read_PMS5003 = "SUCCEED";
-  } else {
-    send_Status_Read_PMS5003 = "FAILED";
-  }
-}
-
-// PM25 aqi value
-int calcAQI25(int pm25) {
-
-  // Uses formula AQI = ( (pobs - pmin) x (aqimax - aqimin) ) / (pmax - pmin)  + aqimin
-  float pmin, pmax, amin, amax;
-
-  if (pm25 <= 12) {
-    pmin = 0;
-    pmax = 12;
-    amin = 0;
-    amax = 50;
-    goto aqicalc;
-  } else if (pm25 <= 35.5) {
-    pmin = 12;
-    pmax = 35.5;
-    amin = 50;
-    amax = 100;
-    goto aqicalc;
-  } else if (pm25 <= 55.5) {
-    pmin = 35.5;
-    pmax = 55.5;
-    amin = 100;
-    amax = 150;
-    goto aqicalc;
-  } else if (pm25 <= 150.5) {
-    pmin = 55.5;
-    pmax = 150.5;
-    amin = 150;
-    amax = 200;
-    goto aqicalc;
-  } else if (pm25 <= 250.5) {
-    pmin = 150.5;
-    pmax = 250.5;
-    amin = 200;
-    amax = 300;
-    goto aqicalc;
-  } else if (pm25 <= 350.5) {
-    pmin = 250.5;
-    pmax = 350.5;
-    amin = 300;
-    amax = 400;
-    goto aqicalc;
-  } else if (pm25 <= 500.5) {
-    pmin = 350.5;
-    pmax = 500.5;
-    amin = 400;
-    amax = 500;
-    goto aqicalc;
-  } else {
-    return 999;
-  }
-
-aqicalc:
-  float aqi = (((pm25 - pmin) * (amax - amin)) / (pmax - pmin)) + amin;
-  return aqi;
-}
-
-void get_aqi_status() {
-  if (PM25_aqival <= 50) {
-    aqi_status = "Good";
-  } else if (PM25_aqival >= 51 && PM25_aqival <= 100) {
-    aqi_status = "Moderate";
-  } else if (PM25_aqival >= 101 && PM25_aqival <= 150) {
-    aqi_status = "Unhealthy for Sensitive Groups";
-  } else if (PM25_aqival >= 151 && PM25_aqival <= 200) {
-    aqi_status = "Unhealthys";
-  } else if (PM25_aqival >= 201 && PM25_aqival <= 300) {
-    aqi_status = "Very Unhealthy";
-  } else if (PM25_aqival >= 300) {
-    aqi_status = "Hazardous";
-  }
-}
-
-void setup() {
-  //Start Serial Monitor
-  Serial.begin(115200);
-
-  //start DHT11 Sensor
-  dht.begin();
-  //start PMS5003 Sensor
-  Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
-
-  //Connect to Wifi Network
-  connect_WiFi();
-}
-
-void loop() {
-  //Reading DHT11 Data
-  get_DHT11_sensor_data();
-  //Reading PMS5003 Data
-  get_PMS5003_sensor_data();
-
-  //Reading data was successful! Print Data's
-  //Output PMS5003 Data
-  Serial.println();
-  Serial.println("\n-------------------------------------");
-  Serial.println("Concentration Units (environmental)");
-  Serial.print("PM 1.0: ");
-  Serial.print(PM1);
-  Serial.print("\t\tPM 2.5: ");
-  Serial.print(PM2_5);
-  Serial.print("\t\tPM 10: ");
-  Serial.print(PM10);
-  Serial.println("\n-------------------------------------");
-  Serial.print("Particules > 0.3 um / 0.1L air:");
-  Serial.println(data.particles_03um);
-  Serial.print("Particules > 0.5 um / 0.1L air:");
-  Serial.println(data.particles_05um);
-  Serial.print("Particules > 1.0 um / 0.1L air:");
-  Serial.println(data.particles_10um);
-  Serial.print("Particules > 2.5 um / 0.1L air:");
-  Serial.println(data.particles_25um);
-  Serial.print("Particules > 5.0 um / 0.1L air:");
-  Serial.println(data.particles_50um);
-  Serial.print("Particules > 10.0 um / 0.1L air:");
-  Serial.println(data.particles_100um);
-  Serial.println("--------------------------------------");
-  //Output DHT11 Data
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.print("ºC ");
-  Serial.print("Humidity: ");
-  Serial.println(humidity);
-  //Get CO Data
-  Serial.print("CO: ");
-  Serial.print(CO);
-  //Get PM2.5 AQI value
-  get_aqi_status();
-  Serial.print("\nPM2.5 AQI value: ");
-  Serial.println(PM25_aqival);
-  //Get AQI Status
-  get_aqi_status();
-  Serial.print("AQI Status: ");
-  Serial.println(aqi_status);
-  Serial.println();
-
-  Serial.print("connecting to ");
-  Serial.println(host);
-
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  const int httpPort = 80;
-  if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
+    Serial.println("SUCCEED");
+    Serial.println();
+    Serial.print("Temperature: ");
+    Serial.print(*y);
+    Serial.print("\t\tHumidity: ");
+    Serial.print(*x);
+    Serial.println("\n---------------------------------------");
     return;
   }
+}
 
-  // This will send the request to the server
-  client.print(String("GET http://ustpapm.000webhostapp.com/api/connect.php?") + ("&PM1=") + PM1 + ("&PM2_5=") + PM2_5 + ("&PM10=") + PM10 + ("&temperature=") + temperature + ("&humidity=") + humidity + ("&CO=") + CO + ("&PM25_aqival=") + PM25_aqival + ("&aqi_status=") + aqi_status + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
-
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 1000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      return;
-    }
-  }
-
-  // Read all the lines of the reply from server and print them to Serial
-  while (client.available()) {
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
-  }
-
+void get_PMS5003_sensor_data(int* a, int* b, int* c) {
   Serial.println();
-  Serial.println("closing connection");
-
-  delay(15000);
+  Serial.println("-----Getting PMS5003 Sensor Data-----");
+  if (readPMSdata(&pmsSerial)) {
+    Serial.println("SUCCEED");
+    // reading data was successful!
+    *a = data.pm10_env;
+    *b = data.pm25_env;
+    *c = data.pm100_env;
+    Serial.println();
+    Serial.println("---------------------------------------");
+    Serial.println("Concentration Units (environmental)");
+    Serial.print("PM 1.0: ");
+    Serial.print(*a);
+    Serial.print("\t\tPM 2.5: ");
+    Serial.print(*b);
+    Serial.print("\t\tPM 10: ");
+    Serial.println(*c);
+    Serial.println("---------------------------------------");
+    Serial.print("Particles > 0.3um / 0.1L air:");
+    Serial.println(data.particles_03um);
+    Serial.print("Particles > 0.5um / 0.1L air:");
+    Serial.println(data.particles_05um);
+    Serial.print("Particles > 1.0um / 0.1L air:");
+    Serial.println(data.particles_10um);
+    Serial.print("Particles > 2.5um / 0.1L air:");
+    Serial.println(data.particles_25um);
+    Serial.print("Particles > 5.0um / 0.1L air:");
+    Serial.println(data.particles_50um);
+    Serial.print("Particles > 10.0 um / 0.1L air:");
+    Serial.println(data.particles_100um);
+    Serial.println("---------------------------------------");
+    return;
+  } else {
+    delay(10000);  //10 seconds delay before system restarts
+    Serial.println("FAILED");
+    reboot();
+  }
 }
 
 boolean readPMSdata(Stream* s) {
@@ -282,12 +154,12 @@ boolean readPMSdata(Stream* s) {
     sum += buffer[i];
   }
 
-  /* debugging
-  for (uint8_t i=2; i<32; i++) {
+  //debugging
+  /*for (uint8_t i=2; i<32; i++) {
     Serial.print("0x"); Serial.print(buffer[i], HEX); Serial.print(", ");
   }
-  Serial.println();
-  */
+  Serial.println();*/
+
 
   // The data comes in endian'd, this solves it so it works on all platforms
   uint16_t buffer_u16[15];
@@ -305,4 +177,176 @@ boolean readPMSdata(Stream* s) {
   }
   // success!
   return true;
+}
+
+// PM25 aqi value
+int calcAQI25(int pm25aqi) {
+
+  // Uses formula AQI = ( (pobs - pmin) x (aqimax - aqimin) ) / (pmax - pmin)  + aqimin
+  float pmin, pmax, amin, amax;
+
+  if (pm25aqi <= 12) {
+    pmin = 0;
+    pmax = 12;
+    amin = 0;
+    amax = 50;
+    goto aqicalc;
+  } else if (pm25aqi <= 35.5) {
+    pmin = 12;
+    pmax = 35.5;
+    amin = 50;
+    amax = 100;
+    goto aqicalc;
+  } else if (pm25aqi <= 55.5) {
+    pmin = 35.5;
+    pmax = 55.5;
+    amin = 100;
+    amax = 150;
+    goto aqicalc;
+  } else if (pm25aqi <= 150.5) {
+    pmin = 55.5;
+    pmax = 150.5;
+    amin = 150;
+    amax = 200;
+    goto aqicalc;
+  } else if (pm25aqi <= 250.5) {
+    pmin = 150.5;
+    pmax = 250.5;
+    amin = 200;
+    amax = 300;
+    goto aqicalc;
+  } else if (pm25aqi <= 350.5) {
+    pmin = 250.5;
+    pmax = 350.5;
+    amin = 300;
+    amax = 400;
+    goto aqicalc;
+  } else if (pm25aqi <= 500.5) {
+    pmin = 350.5;
+    pmax = 500.5;
+    amin = 400;
+    amax = 500;
+    goto aqicalc;
+  } else {
+    return 999;
+  }
+
+aqicalc:
+  float aqi = (((pm25aqi - pmin) * (amax - amin)) / (pmax - pmin)) + amin;
+  return aqi;
+}
+
+const char* get_aqi_status(int PM25_aqival) {
+  if (PM25_aqival <= 50) {
+    return "Good";
+  } else if (PM25_aqival >= 51 && PM25_aqival <= 100) {
+    return "Moderate";
+  } else if (PM25_aqival >= 101 && PM25_aqival <= 150) {
+    return "Unhealthy for Sensitive Groups";
+  } else if (PM25_aqival >= 151 && PM25_aqival <= 200) {
+    return "Unhealthy";
+  } else if (PM25_aqival >= 201 && PM25_aqival <= 300) {
+    return "Very Unhealthy";
+  } else if (PM25_aqival >= 300) {
+    return "Hazardous";
+  }
+}
+
+void setup() {
+  //Start Serial Monitor
+  Serial.begin(115200);
+  while (!Serial) delay(10);
+
+  // Wait to allow sensors to get ready
+  delay(2000);
+
+  //start DHT11 Sensor
+  dht.begin();
+
+  // Set up UART connection
+  pmsSerial.begin(9600, SERIAL_8N1, RXD2, TXD2);
+
+  //Connect to Wifi Network
+  connect_WiFi();
+
+
+  Serial.println("\nAll sensors initialized, 5-second warm-up delay before first reading");
+  delay(5000);
+}
+
+void loop() {
+  int PM1 = 0;
+  int PM2_5 = 0;
+  int PM10 = 0;
+  float humidity = 0.00;
+  float temperature = 0.00;
+  float CO_ppm = 0.00;
+
+  //Reading PMS5003 Data
+  get_PMS5003_sensor_data(&PM1, &PM2_5, &PM10);
+
+  //Reading DHT11 Data
+  get_DHT11_sensor_data(&humidity, &temperature);
+
+  int PM25_aqival = calcAQI25(PM2_5);
+  Serial.print("\nPM2.5 AQI value: ");
+  Serial.println(PM25_aqival);
+
+  const char* aqi_status = get_aqi_status(PM25_aqival);
+  Serial.print("AQI Status: ");
+  Serial.println(aqi_status);
+  Serial.println();
+
+  //Check WiFi connection status
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClientSecure* client = new WiFiClientSecure;
+    client->setInsecure();  //don't use SSL certificate
+    HTTPClient https;
+
+    // Your Domain name with URL path or IP address with path
+    https.begin(*client, serverName);
+
+    // Specify content-type header
+    https.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    // Prepare your HTTP POST request data
+    String httpRequestData = "api_key=" + apiKeyValue + "&PM1=" + PM1 + "&PM2_5=" + PM2_5 + "&PM10=" + PM10 + "&temperature=" + temperature + "&humidity=" + humidity + "&CO_ppm=" + CO_ppm + "&PM25_aqival=" + PM25_aqival + "&aqi_status=" + aqi_status + "";
+    Serial.print("httpRequestData: ");
+    Serial.println(httpRequestData);
+
+    // Send HTTP POST request
+    int httpResponseCode = https.POST(httpRequestData);
+
+    if (httpResponseCode > 0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    } else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    // Free resources
+    https.end();
+  } else {
+    Serial.println("WiFi Disconnected");
+  }
+  Serial.println();
+  Serial.println("closing connection");
+  //flush the serial buffer
+  while (Serial.available()) {
+    Serial.read();
+    Serial.end();
+    Serial.begin(115200);
+  }
+  while (pmsSerial.available()) {
+    pmsSerial.read();
+    pmsSerial.end();
+    pmsSerial.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  }
+
+  delay(20000);  //20 seconds delay before sending new data
+}
+
+void reboot() {
+  Serial.println("\nRestarting Device");
+  ESP.restart();
 }
